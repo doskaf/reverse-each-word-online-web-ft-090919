deede7988408e44b1cf35abce4255269e78da1fd
Dominique Skaf <doskaf@yahoo.com>
2019-09-13 20:14:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(string)
   reversed_string = ""
-  a_string = string.to_a
-  a_string.each do |word|
+  string.each do |word|
     reversed_string << word.reverse
   end
   reversed_string
