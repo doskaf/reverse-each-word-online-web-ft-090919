0bd90a82a609f0a179668d2a91f2924f3ba17160
Dominique Skaf <doskaf@yahoo.com>
2019-09-13 20:19:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,4 @@
 def reverse_each_word(string)
-  reversed_string = []
-  a_string = string.split(" ")
-  a_string.each do |word|
-    reversed_string << word.reverse
-  end
-  reversed_string.join(" ")
-end
-
-def reverse_each_word(string)
   a_string = string.split(" ")
   a_string.collect do |word|
     word.reverse
